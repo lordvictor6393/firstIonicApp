d52a631a3455b76a7b370f4f5f3a7671178f9af5
lordvictor6393 <lordvictor6393@gmail.com>
2018-08-02 19:28:36 -0400

    adding changes for setting local notifications

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, Platform } from 'ionic-angular';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  data = { title: '', description: '', date: '', time: '' };
+
+  constructor(public navCtrl: NavController,
+    public localNotifications: LocalNotifications,
+    public alertCtrl: AlertController,
+    public platform: Platform) {
 
   }
 
+  registerPrayReminder() {
+    console.log(this.data);
+    let date = new Date(this.data.date + " " + this.data.time);
+    console.log(date);
+    this.localNotifications.schedule({
+      text: 'Delayed ILocalNotification',
+      trigger: { at: date },
+      led: 'FF0000',
+      sound: this.setSound(),
+    });
+    let alert = this.alertCtrl.create({
+      title: 'Congratulation!',
+      subTitle: 'Notification setup successfully at ' + date,
+      buttons: ['OK']
+    });
+    alert.present();
+    this.data = { title: '', description: '', date: '', time: '' };
+  }
+
+  setSound() {
+    if (this.platform.is('android')) {
+      return 'file://assets/sounds/Rooster.mp3'
+    } else {
+      return 'file://assets/sounds/Rooster.caf'
+    }
+  }
 }
